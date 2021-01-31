ac215d31561cca5c0603cfbfeb163236d67e1254
Hagar Usama <Hagar_Usama@outlook.com>
2021-01-31 17:26:15 +0200

    add tsk1

---
+++ build/CMakeFiles/2D_feature_tracking.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hagar/Documents/OhMy!/Sensor_Fusion/Camera-Part/SFND_2D_Feature_Tracking/src/MidTermProject_Camera_Student.cpp" "/home/hagar/Documents/OhMy!/Sensor_Fusion/Camera-Part/SFND_2D_Feature_Tracking/build/CMakeFiles/2D_feature_tracking.dir/src/MidTermProject_Camera_Student.cpp.o"
+  "/home/hagar/Documents/OhMy!/Sensor_Fusion/Camera-Part/SFND_2D_Feature_Tracking/src/matching2D_Student.cpp" "/home/hagar/Documents/OhMy!/Sensor_Fusion/Camera-Part/SFND_2D_Feature_Tracking/build/CMakeFiles/2D_feature_tracking.dir/src/matching2D_Student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/2D_feature_tracking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2D_feature_tracking"
+  "2D_feature_tracking.pdb"
+  "CMakeFiles/2D_feature_tracking.dir/src/MidTermProject_Camera_Student.cpp.o"
+  "CMakeFiles/2D_feature_tracking.dir/src/matching2D_Student.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2D_feature_tracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
